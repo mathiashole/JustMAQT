4dae14f6e8243517c6225c0eb939706647b830bb
mathiashole <joacomangino@gmail.com>
2024-08-21 08:47:37 -0300

    changed check if alignment provided in genotype

--- PPHY.R
+++ PPHY.R
@@ -84,10 +84,11 @@ apply_alignment <- function(p, alignment_file, layout_option) {
 # Function to create a gheatmap
 plot_gheatmap <- function(tree_plot, genotype_file, alignment_file, offset = 5, width = 0.5, font_size = 3, 
                           colnames_angle = -45, hjust = 0, color_palette = "Set3") {
-  # Check if alignment file
-  if (alignment_file) {
+
+  # Check if alignment file is provided
+  if (!is.null(alignment_file)) {
     warning("Genotype cannot be applied with alignment file. The function will not be executed.")
-    return(p)
+    return(tree_plot)
   }
 
   if (!is.null(genotype_file)) {
