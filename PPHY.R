cf105b19022ed29fce6f547ec73d98b318b46a33
mathiashole <mangino@fcien.edu.uy>
2024-09-04 10:21:31 -0300

    debug pallete

--- PPHY.R
+++ PPHY.R
@@ -104,6 +104,7 @@ apply_alignment <- function(p, alignment_file, layout_option) {
 
 # Function to create a gheatmap
 # need debug heatmap
+# bug ther are in aplication color pallet
 plot_genotype_heatmap <- function(tree_plot, genotype_file, alignment_file, offset = 5, width = 0.5, font_size = 3, 
                           colnames_angle = -45, hjust = 0, color_palette = "Set3") {
 
