f08abe1a6a2c82db3eec9b603a68328290232e71
mathiashole <mangino@fcien.edu.uy>
2024-11-01 13:04:22 -0300

    removed: arguments option of execution apply_annotations

--- PPHY.R
+++ PPHY.R
@@ -463,7 +463,7 @@ if (cluster_option == "auto") {
   p <- p %<+% data + 
     # geom_tiplab(aes(color = I(color))) +  # Paint species labels
     # geom_tippoint(aes(color= I(color))) + # try point color
-    p <- apply_annotations(p, data, opt$annotations)
+    p <- apply_annotations(p, opt$annotations)
     geom_text2(aes(subset = !isTip, label = label), hjust = -.3) + # Show bootstrap values
     # geom_text(aes(label = node), hjust = -.3) +
     geom_treescale() + 
