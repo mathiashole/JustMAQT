f8229743b72bbc1f93837a7b3d3b136fc1495ef7
mathiashole <mangino@fcien.edu.uy>
2024-11-10 11:10:29 -0300

    add: init function

---
+++ init.R
@@ -0,0 +1,17 @@
+
+# Function to check if a given input is a list of numeric values
+is_numeric_list <- function(input) {
+  # Split the input by spaces
+  elements <- strsplit(input, " ")[[1]]
+  
+  # Check if there is more than one element and all elements are numeric
+  length(elements) > 1 && all(sapply(elements, function(x) !is.na(as.numeric(x))))
+}
+
+# Function to manage packages, installing if not available
+manage_package <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+  library(pkg, character.only = TRUE)
+}
