b3c768194b39c0017ec7f91f1e8fdcfdbae79746
mathiashole <mangino@fcien.edu.uy>
2026-05-15 23:56:19 -0300

    Update validation

--- makeito.R
+++ makeito.R
@@ -62,6 +62,10 @@ dataset_file <- config$dataset$file
 output_file <- config$output$file
 dataset_options <- config$dataset$options %||% list()
 
+# ---- Validate config ----
+
+if (is.null(tree_file) && is.null(ids_file)) stop("You must provide either input$tree or input$ids")
+
 # ---- Initialize variables ----
 tree_file <- NULL
 ids_file <- NULL
