9249b40ec1c5d82023afef32c2dcfdb600eff8a3
mathiashole <mangino@fcien.edu.uy>
2026-05-17 11:43:13 -0300

    Update read tree

--- makeito.R
+++ makeito.R
@@ -76,7 +76,8 @@ if (is.null(output_file)) stop("output$file is required")
 # ---- Read Ids and Tree ----
 
 if (!is.null(tree_file)) {
-
+  
+  tree <- read.tree(tree_file)
 }
 
 # ---- Initialize variables ----
