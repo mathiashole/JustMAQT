6ce85e45f3f09efbf28d55cad85b48556be497f0
mathiashole <mangino@fcien.edu.uy>
2026-05-18 23:26:02 -0300

    Update next section barplot

--- makeito.R
+++ makeito.R
@@ -168,7 +168,7 @@ field_labels_line <- paste("FIELD_LABELS", paste(col_labels, collapse = "\t"), s
 data_lines <- do.call(paste, c(df, sep = "\t"))
 out_lines <- c(final_header, "DATA", data_lines)
 
-}
+} else if (dataset_type == "barplot") {}
 # 1. Keywords mode
 
 # # ---- Initialize variables ----
