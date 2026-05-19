5f561f6afe0c2eeac64659047a35fe953a72840e
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2026-05-19 17:06:19 -0300

    Document boxplot branch with comment
    Insert a '# 5. Boxplot mode' comment in makeito.R to mark the boxplot branch and improve code readability. This is a non-functional change for clarity and navigation within the file.

--- makeito.R
+++ makeito.R
@@ -205,7 +205,7 @@ out_lines <- c(final_header, "DATA", data_lines)
   out_lines <- c(header, "DATA", do.call(paste, c(df, sep = ",")))
 
 } else if (dataset_type == "boxplot") {
-
+  # 5. Boxplot mode
 } 
 # 1. Keywords mode
 
