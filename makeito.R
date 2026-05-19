dadd281536dbb658321613d812a9ce949f39f8c3
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2026-05-19 18:39:26 -0300

    Save column labels from input df
    After reading the input table and replacing NA with empty strings, extract and store column names (excluding the first ID column) into col_labels for downstream use.

--- makeito.R
+++ makeito.R
@@ -210,6 +210,9 @@ out_lines <- c(final_header, "DATA", data_lines)
   df <- read_input_table(dataset_file)
   if (ncol(df) < 3) stop("Boxplot file must have at least 3 columns: ID and at least 2 numeric values.")
   df[is.na(df)] <- ""
+
+  col_labels <- colnames(df)[-1]
+
 } 
 # 1. Keywords mode
 
