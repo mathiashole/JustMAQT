97eec9df19654b022ce97de5bdc517dc7402aedd
mathiashole <mangino@fcien.edu.uy>
2026-05-20 20:53:21 -0300

    Update header field label

--- makeito.R
+++ makeito.R
@@ -222,6 +222,9 @@ out_lines <- c(final_header, "DATA", data_lines)
   field_labels_line <- paste("FIELD_LABELS", paste(col_labels, collapse = ","), sep = ",")
   color_line <- paste("COLOR", pal, sep = ",")
 
+header <- if (any(grepl("^FIELD_LABELS", header))) gsub("^FIELD_LABELS.*", field_labels_line, header) else c(header, field_labels_line)
+
+
 } 
 # 1. Keywords mode
 
